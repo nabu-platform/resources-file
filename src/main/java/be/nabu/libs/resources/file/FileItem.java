933bf0cc95ad1872c7e464f30403027af8aa1c29
alex <alex@nabu.be>
2015-11-05 10:07:47 +0100

    Updated to use java 1.7 Added last accessed data

--- src/main/java/be/nabu/libs/resources/file/FileItem.java
+++ src/main/java/be/nabu/libs/resources/file/FileItem.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.util.Date;
 
+import be.nabu.libs.resources.api.AccessTrackingResource;
 import be.nabu.libs.resources.api.FiniteResource;
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.ResourceContainer;
@@ -17,7 +22,7 @@ import be.nabu.utils.io.api.ByteBuffer;
 import be.nabu.utils.io.api.ReadableContainer;
 import be.nabu.utils.io.api.WritableContainer;
 
-public class FileItem extends FileResource implements ReadableResource, WritableResource, FiniteResource, TimestampedResource {
+public class FileItem extends FileResource implements ReadableResource, WritableResource, FiniteResource, TimestampedResource, AccessTrackingResource {
 
 	public FileItem(ResourceContainer<?> parent, File file) {
 		super(parent, file);
@@ -37,6 +42,12 @@ public class FileItem extends FileResource implements ReadableResource, Writable
 
 	@Override
 	public ReadableContainer<ByteBuffer> getReadable() throws FileNotFoundException {
+		try {
+			Files.setAttribute(getFile().toPath(), "lastAccessTime", FileTime.fromMillis(new Date().getTime()));
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 		return IOUtils.wrap(new FileInputStream(getFile()));
 	}
 
@@ -48,6 +59,17 @@ public class FileItem extends FileResource implements ReadableResource, Writable
 	@Override
 	public Date getLastModified() {
 		return new Date(getFile().lastModified());
+	}
+
+	@Override
+	public Date getLastAccessed() {
+		try {
+			BasicFileAttributes readAttributes = Files.readAttributes(getFile().toPath(), BasicFileAttributes.class);
+			return new Date(readAttributes.lastAccessTime().toMillis());
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
